588ea06d855757e4dcc5e6bd9232d8b1273fa1f6
flb <f.lb@gmx.com>
2020-08-08 01:02:48 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # tiktok2webm
-Downloads Tik Toks from urls and converts them to WebMs. Written for 4chan 
+Downloads Tik Toks from urls and converts them to WebMs. Written for 4chan. Can create both soundless and with sound WebMs
+
+## Requirements
+Requires ffmpeg to be installed. Also required two python modules to be installed
+```
+$ pip install -r requirements.txt
+```
+
+## Usage
+```
+$ python3 tiktok2webm.py --help
+usage: tiktok2webm.py [-h] [-a] [-q QUALITY] [-b BITRATE] [-w WIDTH] url [url ...]
+
+positional arguments:
+  url                   The URL of the Tik Tok
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -a, --audio           Enables audio for the WebM
+  -q QUALITY, --quality QUALITY
+                        Quality of the webm. From 4 (best) to 63 (worst). Defaults to 20.
+  -b BITRATE, --bitrate BITRATE
+                        Bitrate. From 16 (min) to 15000 (max). Defaults to 1000.
+  -w WIDTH, --width WIDTH
+                        Width (in pixels) of the WebM
+```
+
+You can play with the quality/bitrate/image size to adjust the file size.
+
+Multiple Tik Toks can be downloaded by providing multiple ULRs at once.
---
+++ tiktok2webm.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+from TikTokApi import TikTokApi
+from converter import Converter
+import argparse
+import time
+import os
+
+def downloadTikTok(url):
+    api = TikTokApi()
+    data = api.getTikTokByUrl(url)
+    return api.get_Video_By_DownloadURL(data['itemInfo']['itemStruct']['video']['downloadAddr'])
+
+def convertToWebM(video, args):
+    conv = Converter()
+    with open('temp.mp4', 'wb') as out:
+        out.write(video)
+
+    options = {
+        'format': 'webm',
+        'video': {
+            'codec': 'vp8',
+            'quality': args.quality,
+            'max_bitrate': args.bitrate,
+            'width': args.width,
+            'height': -2
+    }}
+    if (args.audio):
+        options['audio'] = {'codec': 'vorbis'}
+    convert = conv.convert('temp.mp4', str(int(time.time()))+'.webm', options)
+
+    for timecode in convert:
+        print(f'\rConverting to WebM ({int(timecode*100)}%)', end =' ')
+    print('\rConverting to WebM (100%)')
+
+    if os.path.exists('temp.mp4'):
+        os.remove('temp.mp4')
+    
+def main(args):
+    for url in args.url:
+        convertToWebM(downloadTikTok(url), args)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('url', nargs='+', help='The URL of the Tik Tok')
+    parser.add_argument('-a', '--audio', action="store_true", help='Enables audio for the WebM')
+    parser.add_argument('-q', '--quality', type=int, help='Quality of the webm. From 4 (best) to 63 (worst). Defaults to 20.')
+    parser.add_argument('-b', '--bitrate', type=int, help='Bitrate. From 16 (min) to 15000 (max). Defaults to 1000.')
+    parser.add_argument('-w', '--width', type=int, help='Width (in pixels) of the WebM')
+    args = parser.parse_args()
+    if args.quality is None:
+        args.quality = 20
+    if args.bitrate is None:
+        args.bitrate = 1000
+    if args.width is None:
+        args.width = 0
+    main(args)
